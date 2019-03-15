4c5cad44d3581b37753f7a814345ed3525f14bf8
Dingo <oferdottan@gmail.com>
2019-03-15 04:17:32 +0200

    Initial release

---
+++ Source/Entry.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using Harmony;
+using UnityEngine;
+
+namespace StorageInfo
+{
+    public class Entry
+    {
+        private static void InitializeHarmony()
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create("dingo.storageinfo");
+
+#if DEBUG
+            HarmonyInstance.DEBUG = true;
+#endif
+
+            MethodInfo containerOnHandHover = AccessTools.Method(typeof(StorageContainer), nameof(StorageContainer.OnHandHover));
+
+            // Removes original SetInteractText and injects SetCustomInteractText
+            harmony.Patch(
+                original: containerOnHandHover,
+                prefix: null,
+                postfix: null,
+                transpiler: new HarmonyMethod(typeof(HarmonyPatches), nameof(HarmonyPatches.Patch_StorageContainer_OnHandHover_Transpiler)));
+        }
+
+        public static void Initialize()
+        {
+            try
+            {
+                InitializeHarmony();
+            }
+
+            catch (Exception ex)
+            {
+                Debug.Log(ex.ToString());
+                Debug.Log("[StorageInfo] :: Error during mod initialization!");
+            }
+        }
+    }
+}
---
+++ Source/Translation.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using LitJson;
+using UnityEngine;
+
+namespace StorageInfo
+{
+    internal static class Translation
+    {
+        private const string LanguagesFolder = "Languages";
+
+        private static readonly Dictionary<string, string> languageStrings = new Dictionary<string, string>();
+
+        private static string GetAssemblyDirectory
+        {
+            get
+            {
+                string fullPath = Assembly.GetExecutingAssembly().Location;
+                return Path.GetDirectoryName(fullPath);
+            }
+        }
+
+        private static bool TryTranslate(string candidate, out string translated)
+        {
+            if (languageStrings.TryGetValue(candidate, out translated))
+            {
+                return true;
+            }
+
+            else
+            {
+                LoadLanguageData();
+
+                if (languageStrings.TryGetValue(candidate, out translated))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        // Json streaming code taken from Language.LoadLanguageFile(string)
+        internal static void LoadLanguageData()
+        {
+            string currentLanguage = Language.main.GetCurrentLanguage();
+
+            if (string.IsNullOrEmpty(currentLanguage))
+            {
+                currentLanguage = "English";
+            }
+
+            string langFolder = Path.Combine(GetAssemblyDirectory, LanguagesFolder);
+            string langFile = Path.Combine(langFolder, currentLanguage + ".json");
+
+            if (!File.Exists(langFile))
+            {
+                throw new Exception("[StorageInfo] :: Could not find language file.");
+            }
+
+            JsonData jsonData;
+
+            using (StreamReader streamReader = new StreamReader(langFile))
+            {
+                try
+                {
+                    jsonData = JsonMapper.ToObject(streamReader);
+                }
+
+                catch (Exception ex)
+                {
+                    Debug.Log(ex.ToString());
+                    Debug.Log($"[StorageInfo] :: Failed while loading language json.");
+
+                    return;
+                }
+            }
+
+            foreach (string key in jsonData.Keys)
+            {
+                languageStrings[key] = (string)jsonData[key];
+            }
+        }
+
+
+        internal static string Translate(this string source)
+        {
+            if (TryTranslate(source, out string translated))
+            {
+                return translated;
+            }
+
+            Debug.Log($"[StorageInfo] :: Could not find translated string for `{source}`");
+
+            return source;
+        }
+
+        internal static string FormatSingle(this string source, string arg0)
+        {
+            string basic = source.Translate();
+
+            if (!string.IsNullOrEmpty(arg0))
+            {
+                try
+                {
+                    return string.Format(basic, arg0);
+                }
+
+                catch (Exception ex)
+                {
+                    Debug.Log(ex.ToString());
+                    Debug.Log($"[StorageInfo] :: Failed to format '{source}' with arg0 `{arg0}'");
+                }
+            }
+
+            return basic;
+        }
+    }
+}
